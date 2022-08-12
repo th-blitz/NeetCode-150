5f2ff53ee5575af269fffd789a0b1cf72f760db0
Preetham Rakshith <61616034+ThBlitz@users.noreply.github.com>
2022-08-12 15:34:33 +0530

    linked list : in rust

---
+++ Linked_Lists/list.rs
@@ -0,0 +1,46 @@
+
+
+
+#[derive(Debug)]
+struct SingleListNode {
+    val: i32,
+    next: Option<Box<SingleListNode>>,
+}
+
+impl SingleListNode {
+    fn new(val: i32) -> SingleListNode {
+        return SingleListNode{ val: val, next: None};
+    }
+}
+
+fn build_list(array: Vec<i32>) -> Option<Box<SingleListNode>> {
+    let mut current = None;
+    
+    for i in (0..array.len()).rev() {
+        let mut new_node = SingleListNode::new(array[i]);
+        new_node.next = current;
+        current = Some(Box::new(new_node));
+    }
+    return current;
+}
+
+fn print_nodes(node: &Option<Box<SingleListNode>>) {
+    match node {
+        &Some(ref node) => {
+            print!("{} > ", node.val);
+            print_nodes(&node.next);
+        },
+        None => {
+            println!("None");
+        },
+    }
+}
+
+fn main() {
+
+    let array: Vec<i32> = vec!(1, 2, 3, 4, 5, 6, 7, 8);
+    let head = build_list(array);
+
+    print_nodes(&head);
+
+}
